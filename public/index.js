5dd54a11ea7cdb757bcc707ffebe5cdbbac44d8a
Ольга <olga.semenyuta@gmail.com>
2024-04-27 00:03:51 +0100

    fix cases animation and grey background

--- public/index.js
+++ public/index.js
@@ -100,73 +100,138 @@ document.addEventListener('DOMContentLoaded', () => {
   //cases animation
 
   const items = document.querySelectorAll('.cases__item');
+  window.addEventListener('scroll', onScroll, false);
 
-  document.addEventListener('wheel', () => {
-    const viewportHeight = window.innerHeight;
+  //Get all the section reference
+  var sectionOne = document.querySelector('.features');
+  var sectionTwo = document.querySelector('#case-1');
+  const sectionTwoContainer = sectionTwo.querySelector('.cases__container');
+  var sectionThree = document.querySelector('#case-2');
+  const sectionThreeContainer = sectionThree.querySelector('cases__container');
+  var sectionFourth = document.querySelector('#case-3');
+  const sectionFourContainer = sectionFourth.querySelector('cases__container');
+  var sectionFifth = document.querySelector('#case-4');
+  var sectionSix = document.querySelector('.insights');
 
-    if (event.deltaY < 0) {
-      for (let i = 0; i < items.length; i++) {
-        const topOffset = items[i].getBoundingClientRect().top;
-        const bottomOffset = items[i].getBoundingClientRect().bottom;
+  //Calculate their individual height
+  var SectionOneHeight = getComputedStyle(sectionOne).height.split('px')[0];
+  var SectionTwoHeight = getComputedStyle(sectionTwo).height.split('px')[0];
+  var SectionThreeHeight = getComputedStyle(sectionThree).height.split('px')[0];
+  var SectionFourthHeight =
+    getComputedStyle(sectionFourth).height.split('px')[0];
+  var SectionFifthHeight = getComputedStyle(sectionFifth).height.split('px')[0];
 
-        if (topOffset >= viewportHeight) {
-          if (items[i - 1]) {
-            items[i - 1].classList.remove('fixed');
-            items[i - 1].classList.add('absolute-top');
-          }
-        } else {
-          // items[i].classList.remove('static');
-          // // items[i].classList.add('absolute');
-          // if (i < items.length - 1) {
-          //   items[i].classList.add('absolute');
-          // }
-        }
-        if (topOffset >= 0 && topOffset < 60 && i === items.length - 1) {
-          items.forEach(item => {
-            item.classList.remove('static');
-            item.classList.add('fixed');
-          });
+  //calculate the checkpoint where item need to be modified
+  var checkPointTwo = sectionTwo.getBoundingClientRect().top;
+  var checkPointThree = sectionThree.getBoundingClientRect().top;
 
-          items[i].classList.add('absolute-bottom');
-          items[i].classList.remove('static');
-          items[i].classList.remove('fixed');
-        }
+  var checkPointFourth = sectionFourth.getBoundingClientRect().top;
 
-        // if (topOffset > viewportHeight) {
-        //   if (items[i - 1]) {
-        //     items[i - 1].classList.add('absolute-top');
-        //     items[i - 1].classList.remove('fixed');
-        //   } else {
-        //     items.forEach(item => {
-        //       item.classList.remove('fixed');
-        //     });
-        //   }
-        // }
-      }
-    } else {
-      for (let i = 0; i < items.length; i++) {
-        const topOffset = items[i].getBoundingClientRect().top;
-        const bottomOffset = items[i].getBoundingClientRect().bottom;
+  var checkPointFifth = sectionFifth.getBoundingClientRect().top;
 
-        if (topOffset <= 0 && bottomOffset >= window.innerHeight) {
-          if (i < items.length - 1) {
-            items[i].classList.add('fixed');
-            items[i].classList.add('changed-background');
-            items[i].classList.remove('absolute');
-          } else {
-            items.forEach(item => {
-              item.classList.add('static');
-              item.classList.remove('absolute-top');
-              item.classList.remove('absolute-bottom');
-              item.classList.remove('fixed');
-
-              item.classList.remove('absolute');
-            });
-          }
-        }
-      }
+  //Scroll logic
+  function onScroll() {
+    var scrollBarPosition = window.pageYOffset;
+    if (scrollBarPosition >= 0 && scrollBarPosition < checkPointTwo) {
+      removeClass(sectionTwo, sectionThree);
+      removeBackground(sectionTwo);
+      removeBackground(sectionThree);
+      removeBackground(sectionFourth);
+    } else if (
+      window.pageYOffset >= checkPointTwo &&
+      window.pageYOffset < checkPointThree
+    ) {
+      addClass(sectionTwo);
+      addBackground(sectionTwo);
+      removeBackground(sectionThree);
+    } else if (
+      window.pageYOffset >= checkPointThree &&
+      window.pageYOffset < checkPointFourth
+    ) {
+      addClass(sectionThree);
+      addBackground(sectionThree);
+      removeBackground(sectionFourth);
+    } else if (
+      window.pageYOffset >= checkPointFourth &&
+      window.pageYOffset < checkPointFifth
+    ) {
+      addClass(sectionFourth);
+      addBackground(sectionFourth);
+    } else if (scrollBarPosition === 0) {
+      removeBackground(sectionTwo);
+      removeBackground(sectionThree);
+      removeBackground(sectionFourth);
     }
-  });
+  }
+  function addClass(elemOne) {
+    elemOne.classList.add('fixed');
+  }
+  function addAbs(elemOne, margin) {
+    elemOne.style.top = margin + 'px';
+    elemOne.style.position = 'absolute';
+  }
+  function addBackground(elem) {
+    elem.classList.add('background-grey');
+  }
+
+  function removeBackground(elem) {
+    elem.classList.remove('background-grey');
+  }
+  function removeClass(elemOne, elemTwo) {
+    elemOne.classList.remove('fixed');
+    elemTwo.classList.remove('absolute');
+  }
+  // document.addEventListener('wheel', () => {
+  //   const viewportHeight = window.innerHeight;
+
+  //   if (event.deltaY < 0) {
+  //     for (let i = 0; i < items.length; i++) {
+  //       const topOffset = items[i].getBoundingClientRect().top;
+  //       const bottomOffset = items[i].getBoundingClientRect().bottom;
+
+  //       if (topOffset >= viewportHeight) {
+  //         if (items[i - 1]) {
+  //           items[i - 1].classList.remove('fixed');
+  //           items[i - 1].classList.add('absolute-top');
+  //         }
+  //       } else {
+  //       }
+  //       if (topOffset >= 0 && topOffset < 60 && i === items.length - 1) {
+  //         items.forEach(item => {
+  //           item.classList.remove('static');
+  //           item.classList.add('fixed');
+  //         });
+
+  //         items[i].classList.add('absolute-bottom');
+  //         items[i].classList.remove('static');
+  //         items[i].classList.remove('fixed');
+  //       }
+  //     }
+  //   } else {
+  //     for (let i = 0; i < items.length; i++) {
+  //       const topOffset = items[i].getBoundingClientRect().top;
+  //       const bottomOffset = items[i].getBoundingClientRect().bottom;
+
+  //       if (topOffset <= 0 && bottomOffset >= window.innerHeight) {
+  //         if (i < items.length - 1) {
+  //           items[i].classList.add('fixed');
+  //           items[i].classList.add('changed-background');
+  //           items[i].classList.remove('absolute');
+  //         } else {
+  //           items.forEach(item => {
+  //             item.classList.add('static');
+  //             item.classList.remove('absolute-top');
+  //             item.classList.remove('absolute-bottom');
+  //             item.classList.remove('fixed');
+
+  //             item.classList.remove('absolute');
+  //           });
+  //         }
+  //       }
+  //     }
+  //   }
+  // });
+
   //burger menu
   const sideMenuItems = document.querySelectorAll(
     '.burger-menu__sidemenu [data-menu]'
